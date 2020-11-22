9e1fa67473d7ccf492a572ce78bfc8c6b381fa7c
twy2016 <whgp1994@163.com>
2020-11-23 00:00:05 +0800

    项目创建

---
+++ eureka-server/src/main/java/com/twy/EurekaServerApplication.java
@@ -0,0 +1,13 @@
+package com.twy;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * @author gongpeng
+ * @date 2020/11/22 23:49
+ */
+@SpringBootApplication
+@EnableEurekaServer
+public class EurekaServerApplication {
+}
